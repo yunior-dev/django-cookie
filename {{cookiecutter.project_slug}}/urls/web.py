d633603c1b2a0758aa694e0cb6c0d70b9e0f2f77
Yunior Espinal <yunior.dev@gmail.com>
2020-10-27 14:29:21 -0400

    Made changes to the routing system and added Media support.

--- {{cookiecutter.project_slug}}/urls/web.py
+++ {{cookiecutter.project_slug}}/urls/web.py
@@ -1,10 +1,6 @@
-# from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    # Django Admin
-    # path('admin/', admin.site.urls),
-
     # Add your web urls here
     path('', include('client.urls')),
 ]
