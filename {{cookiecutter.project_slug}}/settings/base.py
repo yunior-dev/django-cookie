ffad684d8b63ae405d90cb5c7a80dbbc6597c154
Yunior Espinal <yunior.dev@gmail.com>
2020-10-27 14:05:13 -0400

    Update base.py

--- {{cookiecutter.project_slug}}/settings/base.py
+++ {{cookiecutter.project_slug}}/settings/base.py
@@ -141,6 +141,14 @@ STATICFILES_FINDERS = [
 STATIC_URL = '/static/'
 
 
+# MEDIA
+# ------------------------------------------------------------------------------
+# https://docs.djangoproject.com/en/dev/ref/settings/#media-root
+MEDIA_ROOT = str(APPS_DIR / "media")
+# https://docs.djangoproject.com/en/dev/ref/settings/#media-url
+MEDIA_URL = "/media/"
+
+
 # TEMPLATES
 # ------------------------------------------------------------------------------
 # https://docs.djangoproject.com/en/dev/ref/settings/#templates
