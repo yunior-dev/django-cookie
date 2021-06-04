6bb6e96ffc59365104cd5fd4c8e47cbe59ad0326
Yunior Espinal <yunior.dev@gmail.com>
2021-06-04 17:41:20 -0400

    Update urls.py

--- {{cookiecutter.project_slug}}/config/urls.py
+++ {{cookiecutter.project_slug}}/config/urls.py
@@ -5,10 +5,10 @@ from django.urls import include, path
 
 urlpatterns = [
     # Django Admin
-    path('admin/', admin.site.urls),
+    path('admin', admin.site.urls),
 
     # API urls: Make changes to backend in api.py
-    path('api/', include('urls.api')),
+    path('api', include('urls.api')),
 
     # Web urls: Make changes to frontend in web.py
     path('', include('urls.web')),
